8a99d79652fcb44c7be0bc452ce3e1448bd7c86a
geilsonfreire <geilosnfreireleite@gmail.com>
2024-10-16 19:54:51 -0300

    Refatorar caminhos do main.jsx Front-End, script de start no package.json e configurações do Vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    build: {
+        outDir: 'dist', // Diretório de saída
+        sourcemap: true, // Crie sourcemaps para facilitar a depuração
+    },
+    server: {
+        port: 3001, // Porta do servidor de desenvolvimento
+    },
+});
